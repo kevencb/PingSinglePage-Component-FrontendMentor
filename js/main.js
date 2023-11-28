87aaa2130ae1231fd677c32a563af092f07e2928
kevencb <kevcb369@gmail.com>
2023-11-28 17:37:33 -0500

    Actualización CSS y JS

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let btnSubmit = document.getElementById('btnSubmit')
 let textError = document.getElementById('textError')
 
 btnSubmit.addEventListener('click', (e) => {
-    e.preventDefault
+    e.preventDefault()
     validateEmail(inputEmail.value)
 })
 
@@ -11,10 +11,14 @@ function validateEmail(inputEmailValue){
     let expReg = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
     
     if(expReg.test(inputEmailValue)){
-        inputEmail.style.border = '1px solid #4C7BF3'
+        inputEmail.classList.remove('inputError')
         textError.style.visibility = 'hidden'
+        textError.style.marginBottom = '0px'
+        console.log('Good')
     }else{
-        inputEmail.style.border = '1px solid #FF5466'
+        inputEmail.classList.add('inputError')
         textError.style.visibility = 'visible'
+        textError.style.marginBottom = '12px'
+        console.log('Bad')
     }
 }
