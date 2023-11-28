6175a89ca540ac572b4bc42043e921cb69b8661c
kevencb <kevcb369@gmail.com>
2023-11-28 17:44:56 -0500

    Actualización CSS y JS

--- js/main.js
+++ js/main.js
@@ -11,12 +11,12 @@ function validateEmail(inputEmailValue){
     let expReg = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
     
     if(expReg.test(inputEmailValue)){
-        inputEmail.classList.remove('inputError')
+        inputEmail.style.border = '1px solid #4C7BF3'
         textError.style.visibility = 'hidden'
         textError.style.marginBottom = '0px'
         console.log('Good')
     }else{
-        inputEmail.classList.add('inputError')
+        inputEmail.style.border = '1px solid #FF5466'
         textError.style.visibility = 'visible'
         textError.style.marginBottom = '12px'
         console.log('Bad')
